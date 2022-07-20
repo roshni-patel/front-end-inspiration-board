4d8982ffbc9869154c23139fae703986e8459c19
Nikki Torab <nikkitorab@gmail.com>
2022-07-20 07:42:16 -0700

    cleaned code

--- src/components/BoardList.js
+++ src/components/BoardList.js
@@ -20,60 +20,10 @@ const BoardList = (props) => {
       });
   };
 
-  // const getCardsFromBoard = () => {
-  //   axios
-  //     // .get(`${process.env.REACT_APP_BACKEND_URL}/boards/${id}/cards`)
-  //     .get(`${process.env.REACT_APP_BACKEND_URL}/boards/${boardID}/cards`)
-  //     .then((response) => {
-  //       setCardsData(response.data);
-  //     })
-  //     .catch((error) => {
-  //       console.log("error getting cards from board");
-  //     });
-  // };
-
-  // const boardComponents = props.boardsData.map((board) => {
-  //   return (
-  //     <li key={board.boardID}>
-  //       <Board
-  //         boardID={board.boardID}
-  //         title={board.title}
-  //         owner={board.owner}
-  //         isSelected={board.isSelected}
-  //         onUpdateSelectedBoard={props.onUpdateSelectedBoard}
-  //         getBoardsFromAPI={props.getBoardsFromAPI}
-  //         deleteBoard={props.deleteBoard}
-  //       ></Board>
-  //       <button
-  //         className="select-board"
-
-  //         onClick={() => onBoardTitleSelect(board.boardID)}
-  //       >
-  //         {board.title}
-  //       </button>
-  //     </li>
-  //   );
-  // });
-
   const onBoardTitleSelect = (id) => {
-    // const selected = props.boardsData.filter((board) => board.boardID === id);
-    // let selected;
     for (const board of props.boardsData) {
-      // console.log(board);
-      // console.log(`id: ${id}, boardID: ${board.boardID}`);
       if (board.boardID === id) {
-        // console.log(`selected: ${board.boardID}`);
-        // selected = (
-        //   <Board
-        //     boardID={board.boardID}
-        //     title={board.title}
-        //     owner={board.owner}
-        //   ></Board>
-        // );
         getCardsFromBoard(id);
-
-        // console.log(`selected: ${board}`);
-        console.log(board);
         setSelectedBoard(board);
         break;
       }
@@ -98,10 +48,7 @@ const BoardList = (props) => {
       <section>
         <h2 className="board-list-label">Boards</h2>
         <ul className="board-list"> {boardButtons}</ul>
-        {/* <h1 className="board-title">{title}</h1>
-      <button onClick={() => props.deleteBoard(boardID)}>Delete Board</button> */}
       </section>
-      {/* <section className="board">{selectedBoard}</section> */}
       <section className="board">
         {selectedBoard && (
           <Board
